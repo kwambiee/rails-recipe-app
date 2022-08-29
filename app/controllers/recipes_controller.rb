b0b575b4cb8507d2cfb23e0b8e7635867c6b1b8c
kwambiee <kwambokaj2.jk@gmail.com>
2022-08-29 18:16:56 +0300

    Add destroy action to recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,47 +11,10 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
-  # # GET /recipes/new
-  # def new
-  #   @recipe = Recipe.new
-  # end
-
-  # # GET /recipes/1/edit
-  # def edit
-  # end
-
-  # # POST /recipes or /recipes.json
-  # def create
-  #   @recipe = Recipe.new(recipe_params)
-
-  #   respond_to do |format|
-  #     if @recipe.save
-  #       format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully created." }
-  #       format.json { render :show, status: :created, location: @recipe }
-  #     else
-  #       format.html { render :new, status: :unprocessable_entity }
-  #       format.json { render json: @recipe.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # # PATCH/PUT /recipes/1 or /recipes/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @recipe.update(recipe_params)
-  #       format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully updated." }
-  #       format.json { render :show, status: :ok, location: @recipe }
-  #     else
-  #       format.html { render :edit, status: :unprocessable_entity }
-  #       format.json { render json: @recipe.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
   # # DELETE /recipes/1 or /recipes/1.json
   def destroy
+    @recipe = Recipe.find(params[:id])
     @recipe.destroy
-
     respond_to do |format|
       format.html { redirect_to recipes_url, notice: "Recipe was successfully destroyed." }
       format.json { head :no_content }
