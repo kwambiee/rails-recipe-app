49c6d545298ee9f9e6f6085c41fa256b9baf5cc5
kwambiee <kwambokaj2.jk@gmail.com>
2022-08-29 13:06:04 +0300

    Add authentication page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
+
+  resources :users, only: [:index] do
+    resources :recipes, only: [:index, :show, :destroy]
+    resources :foods, only: [:index, :show, :new, :create, :destroy]
+  end
 end
