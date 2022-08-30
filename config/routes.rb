fd396222f5e2687b484228a848b06a115eeb0b46
kwambiee <kwambokaj2.jk@gmail.com>
2022-08-30 08:27:14 +0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  resources :recipes
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "users#index"
 
-  resources :users,only:[:index, :show] do
-    resources :recipes, only: [:index, :show, :destroy]
-    resources :foods, only: [:index, :show, :destroy]
-  end
+  resources :users,only:[:index, :show]
+  resources :recipes, only: [:index, :show, :destroy]
+  resources :foods, only: [:index, :show, :destroy]
+
 end
