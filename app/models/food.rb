584d4f9eed78da196a0583eb1441477023ee9144
kwambiee <kwambokaj2.jk@gmail.com>
2022-09-01 11:36:14 +0300

    Fix linters

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,5 +3,4 @@ class Food < ApplicationRecord
   has_many :recipes, through: :recipe_foods, foreign_key: :food_id, class_name: 'Recipe', dependent: :destroy
   has_many :inventory_foods, foreign_key: :food_id, class_name: 'InventoryFood', dependent: :destroy
   has_many :inventories, through: :inventory_foods, foreign_key: :food_id, class_name: 'Inventory', dependent: :destroy
- 
 end
