e41a106d94a8ab6f055be4a8f08049e70d4cff21
kwambiee <kwambokaj2.jk@gmail.com>
2022-09-01 18:20:17 +0300

    Test inventory page

---
+++ spec/features/inventory_index_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature 'Inventory index page' do
+  feature 'shows inventories' do
+
+
+    background do
+        visit new_user_session_path
+      @user1 = User.create!(name: 'kwamboka', email: 'kwambokaj1@gmail.com', password: 'kwamboka')
+    @inventory = Inventory.create(name: 'Recipe one', description:'Inventory one',user_id: @user1.id)
+
+        fill_in 'Email', with: @user1.email
+        fill_in 'Password', with: @user1.password
+
+      click_button 'Log in'
+
+      visit inventories_path
+
+    end
+
+    scenario 'shows the inventory' do
+        expect(page).to have_content(@inventory.name)
+    end
+
+    scenario 'shows the inventory description' do
+        expect(page).to have_content(@inventory.description)
+    end
+
+end
+end
