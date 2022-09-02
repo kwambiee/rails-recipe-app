4f9547a346f7b242f9312a9d40280b1638688f9b
kwambiee <kwambokaj2.jk@gmail.com>
2022-09-02 08:55:02 +0300

    Set up shopping list page

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,8 +1,12 @@
 class ShoppingListsController < ApplicationController
   def index
-    @recipe = Recipe.find(params[:id])
-    @inventories = @recipe.user.inventories.pluck(:name, :id)
-    @quantity = @recipe.recipe_foods.pluck(:quantity)
+    @recipe = Recipe.find(params[:recipe_id])
+    @inventory = Inventory.find(params[:inventory_id])
+    @unlisted_foods = unlisted_foods(params[:recipe_id], params[:inventory_id])
+    total = 0
+    @unlisted_foods.each { |food| total += food[2] }
+    @total = total
+
   end
 
   def create
@@ -13,7 +17,7 @@ class ShoppingListsController < ApplicationController
           # success message
           flash[:success] = 'shopping list saved successfully'
           # redirect to index
-          redirect_to shopping_lists_path(params[:recipe_id])
+          redirect_to shopping_lists_path(recipe_id: shopping_list_params[:recipe_id], inventory_id: shopping_list_params[:inventory_id])
         else
           # error message
           flash.now[:error] = 'Error: shopping list could not be saved'
@@ -28,5 +32,14 @@ class ShoppingListsController < ApplicationController
 
   def shopping_list_params
     params.require(:shopping_list).permit(:recipe_id, :inventory_id)
+  end
+
+  def unlisted_foods(r_id, i_id)
+    inv_food =InventoryFood.joins(:inventory,:food).where(inventory_id: i_id).pluck('foods.id')
+    rec_food = RecipeFood.joins(:recipe,:food).where(recipe_id: r_id).pluck('foods.id')
+    common_foods = inv_food.concat(rec_food)
+    duplicates = common_foods.select { |e| common_foods.count(e) > 1 }.uniq
+    InventoryFood.joins(:food).where.not(food_id: duplicates).pluck('foods.name','inventory_foods.quantity','foods.price')
+
   end
 end
