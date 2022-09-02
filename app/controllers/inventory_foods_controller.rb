b4ad7de6decbb5d0b43feaee6b7bb991a64538e4
kwambiee <kwambokaj2.jk@gmail.com>
2022-09-02 16:32:38 +0300

    Fix linters

--- app/controllers/inventory_foods_controller.rb
+++ app/controllers/inventory_foods_controller.rb
@@ -6,14 +6,13 @@ class InventoryFoodsController < ApplicationController
   end
 
   def create
-  @inventory_food = InventoryFood.new(food_params)
+    @inventory_food = InventoryFood.new(food_params)
     if @inventory_food.save
 
       redirect_to inventory_path(food_params[:inventory_id]), notice: 'Successfully added the food.'
     else
       render :new, locals: { inventory_food: @inventory_food }
     end
-
   end
 
   def destroy
@@ -23,6 +22,7 @@ class InventoryFoodsController < ApplicationController
   end
 
   private
+
   def food_params
     params.permit(:quantity, :food_id, :inventory_id)
   end
